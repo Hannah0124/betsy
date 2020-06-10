e0805f0ab335f72ccca9f82ab6bbc83ec1b4531f
mulhoo <oliviamulholland94@gmail.com>
2020-06-10 14:59:21 -0700

    initial user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "initialize" do
+    before do
+      @new_user = User.new(name: "Marina", email: "marina@ajonisle.com", uid: 43523)
+    end
+
+    it "can be instantiated" do
+      expect(@new_user.valid?).must_equal true
+    end
+    
+    it "has required fields" do
+      [:name, :email, :uid].each do |field|
+        expect(@new_merchant).must_respond_to field
+    end
+  end
 end

e6f3fed4b92ca6855becaf7fa2d74f1b29ceb90f
Corinna Fabre <corinna.fabre@gmail.com>
2020-06-11 13:13:03 -0700

    Minor css updates and fixed seeds

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2020_06_11_011345) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "emaiL_address"
+    t.string "email_address"
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -73,5 +73,4 @@ ActiveRecord::Schema.define(version: 2020_06_11_011345) do
     t.string "provider"
   end
 
-  add_foreign_key "products", "users"
 end

f165daeff83ec7844356f84e0dbfd9909fb7a2d4
Hannah <hannahjoo24@gmail.com>
2020-06-10 22:53:06 -0700

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -68,14 +68,10 @@ ActiveRecord::Schema.define(version: 2020_06_11_011345) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "products_id"
     t.string "photo_url"
     t.integer "uid"
     t.string "provider"
-    t.index ["products_id"], name: "index_users_on_products_id"
   end
 
-
-  add_foreign_key "users", "products", column: "products_id"
   add_foreign_key "products", "users"
 end

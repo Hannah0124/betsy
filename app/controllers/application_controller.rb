c57d034314c8b58a78d6239494b54e0407fb49f2
Hannah <hannahjoo24@gmail.com>
2020-06-10 23:42:42 -0700

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :find_user 
+
+  def render_404
+    # DPR: this will actually render a 404 page in production
+    raise ActionController::RoutingError.new("Not Found")
+  end
+
+  private 
+
+  def find_user 
+    if session[:user_id]
+      @login_user = User.find_by(id: session[:user_id])
+    end
+  end
 end

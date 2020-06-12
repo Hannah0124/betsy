e2a911a1078e4ed9d9d078121d89ebbe7c3e8fdc
Hannah <hannahjoo24@gmail.com>
2020-06-11 21:13:40 -0700

    Add  product_id and reviewer to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_011345) do
+ActiveRecord::Schema.define(version: 2020_06_12_034832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,9 @@ ActiveRecord::Schema.define(version: 2020_06_11_011345) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "product_id"
+    t.string "reviewer"
+    t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -73,4 +76,5 @@ ActiveRecord::Schema.define(version: 2020_06_11_011345) do
     t.string "provider"
   end
 
+  add_foreign_key "products", "users"
 end

08977796d23db7be591542eed960e17fd43921e7
Corinna Fabre <corinna.fabre@gmail.com>
2020-06-13 12:56:27 -0700

    Cart is now hash, cart page populated with additional information, fixed route bugs.

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -8,10 +8,20 @@ class OrderitemsController < ApplicationController
 
   def create
     if !session[:cart]
-      session[:cart] = Array.new
+      session[:cart] = Hash.new
     end 
 
-    session[:cart] << params["product_id"]
+    product = Product.find_by(id: params["product_id"]).id
+    quantity = params["quantity"].to_i
+
+    # session[:cart] << product
+
+    if session[:cart][product]
+      session[:cart][product] += quantity
+    else
+      session[:cart][product] = quantity
+    end
+
     flash[:status] = :success
     flash[:result_text] = "Item added to shopping cart."
     redirect_to product_path(params["product_id"])

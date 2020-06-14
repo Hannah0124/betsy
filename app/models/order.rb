c72350d845d77493f74df355f84c5087150ea6cc
mulhoo <oliviamulholland94@gmail.com>
2020-06-14 00:11:47 -0700

    orders column, name

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,17 +5,18 @@ class Order < ApplicationRecord
   has_many :products, through: :order_items
   
   validates :status, presence: true    
-  validates :name, presence: { message: "Name can't be blank"}, :on => :update
-  validates :email, format: { with: /\A.+@.+\..{2,3}\z/, message: "E-mail address must be valid"}, :on => :update
+  validates :name, presence: {message: "Name can't be blank"}, :on => :update
+  validates :email, format: {with: /\A.+@.+\..{2,3}\z/, message: "E-mail address must be valid"}, :on => :update
   validates :address, presence: {message: "Address can't be blank"}, :on => :update
-  validates :city, presence: { message: "City can't be blank"}, :on => :update
-  validates :state, format: { with: /\A[a-zA-Z]{2}\z/, message: "State must be two letters in length"}, :on => :update
-  validates :zipcode, format: { with: /\A\d{5}\z/, message: "Zip code must be 5 digits" }, :on => :update
-  validates :cc_num, format: { with: /\A\d{13,16}\z/, message: "Credit card number must be 13-16 numbers in length" }, :on => :update, 
-  validates :cc_exp_month, format: {with: /\A\^(0?[1-9]|1[012])$\z/, message: "month must be between 1 and 12"}
-  validates :cc_exp_year, format: { with: /\A\^\d{4}$\z/, message: "year must be 4 digits"} :on => :update
+  validates :city, presence: {message: "City can't be blank"}, :on => :update
+  validates :state, format: {with: /\A[a-zA-Z]{2}\z/, message: "State must be two letters in length"}, :on => :update
+  validates :zipcode, format: {with: /\A\d{5}\z/, message: "Zip code must be 5 digits"}, :on => :update
+  validates :cc_num, format: {with: /\A\d{13,16}\z/, message: "Credit card number must be 13-16 numbers in length"}, :on => :update 
+  validates :cc_exp_month, format: {with: /\A\^(0?[1-9]|1[012])$\z/, message: "month must be between 1 and 12"}, :on => :update
+  validates :cc_exp_year, format: {with: /\A\^\d{4}$\z/, message: "year must be 4 digits"}, :on => :update
+  validates :cc_cvv, format: {with: /\A\d{3,4}\z/, message: "Credit card CVV must be 3-4 numbers in length"}, :on => :update 
+  
   validate :card_expired_check
-  validates :cc_cvv, format: { with: /\A\d{3,4}\z/, message: "Credit card CVV must be 3-4 numbers in length" }, :on => :update  
 
   def total
     order_total = self.orderitems.sum do |orderitem|
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_14_064542) do
+ActiveRecord::Schema.define(version: 2020_06_14_071003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_06_14_064542) do
     t.string "cc_exp_year"
     t.string "cc_cvv"
     t.datetime "order_date"
+    t.string "name"
   end
 
   create_table "products", force: :cascade do |t|

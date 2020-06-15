7448e58a7f791e6284717acd3c100a5e70246c51
Hannah <hannahjoo24@gmail.com>
2020-06-14 21:51:04 -0700

    Modify review_seeds.rb & csv

--- db/review_seeds.rb
+++ db/review_seeds.rb
@@ -6,12 +6,21 @@ require 'date'
 
 CSV.open("db/seeds/review_seeds.csv", "w", :write_headers => true, :headers => ["rating","description","reviewer","product_id", "created_at"]) do |row|
   600.times do 
-    rating = rand(1..5)
+    rating = rand(3..5)
     description = Faker::Quote.famous_last_words
     reviewer = Faker::Name.name
-    product_id = rand(1..300)
+    product_id = rand(1..150)
     created_at = Faker::Time.between(from: Date.today - 120, to: Date.today) 
 
-    row << [rating, description, reviewer,product_id, created_at]
+    row << [rating, description, reviewer, product_id, created_at]
+  end
+
+  25.times do 
+    rating = 5
+    description = Faker::Quote.famous_last_words
+    reviewer = Faker::Name.name
+    product_id = 663
+    created_at = Faker::Time.between(from: Date.today - 120, to: Date.today)
+    row << [rating, description, reviewer, product_id, created_at]
   end
 end 

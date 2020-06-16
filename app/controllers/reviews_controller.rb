e0fbe83322d6b71e0176c91319d5ea1c9ab52111
Hannah <hannahjoo24@gmail.com>
2020-06-15 21:58:10 -0700

    Fix bugs in reviews_controller_test.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,58 +1,55 @@
 class ReviewsController < ApplicationController
-#   # before_action :find_review, only: [:show]
-#   # around_action :render_404, only: [:show], if: -> { @review.nil? }
-
-#   # def index 
-#   #   @reviews = Review.all
-#   # end
-
-#   # def show 
-#   # end
-
-
-  def new
-    @review = Review.new
-    @product = find_product
-  end
-
-  def create
-    product = Product.find_by(id: find_product.id)
-
-    if @login_user && (@login_user.id == product.user_id)
-      flash[:error] = "A problem occurred: Cannot add a review for your own product!"
-      redirect_to product_path(product.id)
-      return
-    else
-
-    if @login_user 
-      @review = Review.new(
-        rating: params[:rating],
-        description: params[:description],
-        product_id: params[:product_id],
-        reviewer: @login_user.name
-      )
-    else 
-      @review = Review.new(review_params) 
+    FIELDS = [:rating, :description, :product_id, :reviewer]
+  
+    def new
+      @review = Review.new
+      @product = find_product
     end
-    
-    if @review.save
-      flash[:success] = "The review was successfully added! 😄"
-    else
-      flash[:error] = "A problem occurred: Could not update the review"
+  
+    def create
+      product = find_product # Product.find_by(id: find_product.id)
+  
+      
+      if @login_user && (@login_user.id == product.user_id)
+        flash[:error] = "A problem occurred: Cannot add a review for your own product!"
+        redirect_to product_path(product.id)
+        return
+
+      else
+
+        if @login_user 
+          @review = Review.new(review_params) 
+          @review.reviewer = @login_user.name
+        else 
+          @review = Review.new(review_params) 
+        end
+      
+        if @review.save
+          flash[:success] = "The review was successfully added! 😄"
+        else
+          messages = [] # TODO 
+
+          FIELDS.each do |field|
+            if @review.errors.messages.include?(field)
+              messages << "'#{field.capitalize}' - #{@review.errors.messages[field].join(", ")}"
+            end
+          end
+
+          flash[:error] = "A problem occurred: Could not update the review \n\n#{messages.join(", ")}"
+        end 
+  
+        redirect_back(fallback_location: root_path)
+        return
+      end
     end
-
-    redirect_back(fallback_location: root_path)
-    return
+  
+    private
+  
+    def review_params
+      return params.permit(*FIELDS)
+    end
+  
+    def find_product
+      return Product.find_by(id: params[:product_id])
+    end
   end
-end
-
-  private
-
-  def review_params
-    params.permit(:rating, :description, :product_id, :reviewer)
-  end
-
-  def find_product
-    return Product.find_by(id: params[:product_id])
-  end
-end
